142f5c1a2586ab010a8ed999c43382253ea81797
SvitlanaChudak <svit.lemon@gmail.com>
2023-01-12 13:34:19 +0200

    Add App, ContactForm

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,44 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
+import React, { Component } from 'react';
+import ContactForm from './ContactForm/ContactForm'
+
+export class App extends Component {
+
+state = {
+    contacts: [],
+    filter: '',
+    }
+    
+    formSubmit = data => {
+    console.log(data)
+}
+
+    render() {
+        return (
+            <>
+                <h1>Phonebook</h1>
+                <ContactForm onSubmit={ this.formSubmit } />
+                <h2>Contacts</h2>
+                <label>Find contacts by name</label>
+                <input type="text" />
+                <ul></ul>
+            </>
+    )
+}
+
+}
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+class ContactForm extends Component {
+    state = {
+        name: '',
+        number: '',
+    };
+
+        inputChange = event => {
+        const { name, value } = event.currentTarget;
+        this.setState({ [name]: value, });
+    };
+
+    inputSubmit = event => {
+        event.preventDefault();
+
+        this.props.onSubmit(this.state);
+
+        this.reset();
+    }
+
+    reset = () => {
+        this.setState({name: '', number: ''})
+    }
+
+    nameInputId = nanoid();
+
+    render() {
+        return (
+            <form onSubmit={this.inputSubmit}>
+                    <label htmlFor={this.nameInputId}>Name</label>
+                    <input
+                     type="text"
+                     name="name"
+                     id={this.nameInputId}
+                     pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                     title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                     required
+                     value={this.state.name}
+                     onChange={this.inputChange}
+                />
+                    <label>Number</label>
+                    <input
+                     type="tel"
+                     name="number"
+                     pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                     title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                     required
+                     value={this.state.number}
+                     onChange={this.inputChange}
+/>
+                    <button type="submit">Add contact</button>
+                </form>
+        )
+    }
+}
+
+export default ContactForm;
